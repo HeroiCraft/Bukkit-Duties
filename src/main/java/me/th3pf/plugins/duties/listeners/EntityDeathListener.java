0da5095f12b8ef02e659efdf8255d96f4be171a4
Randomfish132 <a12345678910ster@gmail.com>
2016-04-14 23:07:59 +0800

    Reformat code.

--- src/main/java/me/th3pf/plugins/duties/listeners/EntityDeathListener.java
+++ src/main/java/me/th3pf/plugins/duties/listeners/EntityDeathListener.java
@@ -1,7 +1,6 @@
 package me.th3pf.plugins.duties.listeners;
 
 import me.th3pf.plugins.duties.Duties;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,23 +9,23 @@ import org.bukkit.event.entity.EntityDeathEvent;
 
 public class EntityDeathListener implements Listener
 {
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onEntityDeath(EntityDeathEvent event) 
-	{
-		if(Duties.Config.GetBoolean("Actions.DisableDeathDrops")
-				&& (event.getEntity() instanceof Player)
-				&& (Duties.Memories.containsKey( ((Player)event.getEntity()).getName()) ))
-		{	
-			event.getDrops().clear();
-			event.setDroppedExp(0);
-		}
-		
-		if(Duties.Config.GetBoolean("Actions.DisableKillDrops")
-				&& (event.getEntity().getKiller() instanceof Player)
-				&& (Duties.Memories.containsKey( event.getEntity().getKiller().getName() )))
-		{
-			event.getDrops().clear();
-			event.setDroppedExp(0);
-		}
-	}
+   @EventHandler( priority = EventPriority.HIGH )
+   public void onEntityDeath( EntityDeathEvent event )
+   {
+      if( Duties.Config.GetBoolean( "Actions.DisableDeathDrops" )
+              && ( event.getEntity() instanceof Player )
+              && ( Duties.Memories.containsKey( ( (Player) event.getEntity() ).getName() ) ) )
+      {
+         event.getDrops().clear();
+         event.setDroppedExp( 0 );
+      }
+
+      if( Duties.Config.GetBoolean( "Actions.DisableKillDrops" )
+              && ( event.getEntity().getKiller() instanceof Player )
+              && ( Duties.Memories.containsKey( event.getEntity().getKiller().getName() ) ) )
+      {
+         event.getDrops().clear();
+         event.setDroppedExp( 0 );
+      }
+   }
 }
