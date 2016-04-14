9a7201296b9f4a0d1ab190e3aca664e17069cfa9
Randomfish132 <a12345678910ster@gmail.com>
2016-04-15 01:27:36 +0800

    Cleanup broadcast action.

--- src/main/java/me/th3pf/plugins/duties/actions/BroadcastAction.java
+++ src/main/java/me/th3pf/plugins/duties/actions/BroadcastAction.java
@@ -1,69 +1,50 @@
 package me.th3pf.plugins.duties.actions;
 
 import me.th3pf.plugins.duties.Duties;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-public class BroadcastAction implements Action //TODO: Cleanup
+public class BroadcastAction implements Action
 {
+   private void broadcast( Player player, String message )
+   {
+      if( !Duties.Config.GetBoolean( "Broadcast-duty-changes" ) )
+         return;
+
+      if( !player.hasPermission( "duties.broadcast" ) )
+         return;
+
+      if( Duties.Hidden.contains( player ) )
+         return;
+
+      String formattedName = player.getName();
+
+      if( Duties.Config.GetBoolean( "Vault.NameFormatting" ) && Bukkit.getPluginManager().isPluginEnabled( "Vault" ) )
+         formattedName = Duties.VaultAdapter.chat.getPlayerPrefix( player ) + formattedName + Duties.VaultAdapter.chat.getPlayerSuffix( player );
+
+      for( Player receivingPlayer : Bukkit.getOnlinePlayers() )
+      {
+         if( receivingPlayer == player )
+            continue;
+
+         if( receivingPlayer.hasPermission( "duties.getbroadcasts" ) )
+            receivingPlayer.sendMessage( message.replaceAll( "%PLAYER_NAME%", formattedName ) );
+      }
+   }
+
    @Override
    public boolean onEnable( Player player )
    {
-      //Broadcasts duty change
-      try
-      {
-         if( Duties.Config.GetBoolean( "Broadcast-duty-changes" ) && player.hasPermission( "duties.broadcast" ) && !Duties.Hidden.contains( player ) )
-         {
-            String FormattedName = player.getName();
+      broadcast( player, Duties.Messages.GetString( "Client.Broadcast.Enabled" ) );
 
-            if( Duties.Config.GetBoolean( "Vault.NameFormatting" ) && Duties.GetInstance().getServer().getPluginManager().isPluginEnabled( "Vault" ) )
-            {
-               FormattedName = ( Duties.VaultAdapter.chat.getPlayerPrefix( player ) + FormattedName + Duties.VaultAdapter.chat.getPlayerSuffix( player ) );
-            }
-
-            for( Player receivingPlayer : Duties.GetInstance().getServer().getOnlinePlayers() )
-            {
-               if( receivingPlayer != player && receivingPlayer.hasPermission( "duties.getbroadcasts" ) )
-                  receivingPlayer.sendMessage( Duties.Messages.GetString( "Client.Broadcast.Enabled" ).replaceAll( "%PLAYER_NAME%", FormattedName ) );
-            }
-         }
-
-         return true;
-      }
-      catch( Exception exception )
-      {
-         Duties.GetInstance().LogMessage( "Failed while broadcasting duty mode change: " + exception.getMessage() );
-         return false;
-      }
+      return true;
    }
 
    @Override
    public boolean onDisable( Player player )
    {
-      //Broadcasts duty mode change
-      try
-      {
-         if( Duties.Config.GetBoolean( "Broadcast-duty-changes" ) && player.hasPermission( "duties.broadcast" ) && !Duties.Hidden.contains( player ) )
-         {
-            String FormattedName = player.getName();
+      broadcast( player, Duties.Messages.GetString( "Client.Broadcast.Disabled" ) );
 
-            if( Duties.Config.GetBoolean( "Vault.NameFormatting" ) && Duties.GetInstance().getServer().getPluginManager().isPluginEnabled( "Vault" ) )
-            {
-               FormattedName = ( Duties.VaultAdapter.chat.getPlayerPrefix( player ) + FormattedName + Duties.VaultAdapter.chat.getPlayerSuffix( player ) );
-            }
-
-            for( Player receivingPlayer : Duties.GetInstance().getServer().getOnlinePlayers() )
-            {
-               if( receivingPlayer != player && receivingPlayer.hasPermission( "duties.getbroadcasts" ) )
-                  receivingPlayer.sendMessage( Duties.Messages.GetString( "Client.Broadcast.Disabled" ).replaceAll( "%PLAYER_NAME%", FormattedName ) );
-            }
-         }
-
-         return true;
-      }
-      catch( Exception exception )
-      {
-         Duties.GetInstance().LogMessage( "Failed while broadcasting duty mode change: " + exception.getMessage() );
-         return false;
-      }
+      return true;
    }
 }
